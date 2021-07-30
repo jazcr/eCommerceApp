f5802d0b139c786c0b924789d53a5ad47f745bdf
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-07-29 21:59:34 -0600

    tag model complete

--- models/Tag.js
+++ models/Tag.js
@@ -1,13 +1,20 @@
-const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
+
   {
     sequelize,
     timestamps: false,
@@ -15,6 +22,8 @@ Tag.init(
     underscored: true,
     modelName: 'tag',
   }
+  
 );
 
 module.exports = Tag;
+
